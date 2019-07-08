85beae9fa7f8a3bec72b04ac283ce303c63d5d1b
xzlyf <czr2001@outlook.com>
2019-07-08 23:21:45 +0800

    修复自定义控件加载失败问题

--- app/src/main/java/publi/xz/com/smartcoupon/ui/custom/SearchBar.java
+++ app/src/main/java/publi/xz/com/smartcoupon/ui/custom/SearchBar.java
@@ -1,14 +1,56 @@
 package publi.xz.com.smartcoupon.ui.custom;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import publi.xz.com.smartcoupon.R;
 
-public class SearchBar extends LinearLayout {
+public class SearchBar extends LinearLayout implements View.OnClickListener {
+    private EditText searchInput;
+    private ImageView searchDelete;
+    private TextView searchBtn;
+
+
+
     public SearchBar(Context context) {
         super(context);
-        LayoutInflater.from(context).inflate(R.layout.custom_main_bar,null);
+        LayoutInflater.from(context).inflate(R.layout.custom_main_bar,this);
+    }
+
+    public SearchBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.custom_main_bar,this);
+
+        init();
+
+
+    }
+
+    private void init() {
+        searchInput = findViewById(R.id.search_input);
+        searchDelete = findViewById(R.id.search_delete);
+        searchBtn = findViewById(R.id.search_btn_a);
+        searchBtn.setOnClickListener(this);
+        searchDelete.setOnClickListener(this);
+        searchInput.clearFocus();//清楚焦点
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.search_btn_a:
+                break;
+            case R.id.search_delete:
+                searchInput.setText("");
+                break;
+        }
     }
 }
