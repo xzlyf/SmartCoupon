26681d8585951ccc54bcfcb6e151a2da002d4da6
czr <czr2001@outlook.com>
2019-07-09 14:02:24 +0800

    主页banner完成

--- app/src/main/java/publi/xz/com/smartcoupon/ui/MainActivity.java
+++ app/src/main/java/publi/xz/com/smartcoupon/ui/MainActivity.java
@@ -1,16 +1,25 @@
 package publi.xz.com.smartcoupon.ui;
 
 import android.content.Intent;
-import android.support.v7.widget.Toolbar;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.Button;
 
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.listener.OnBannerListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import publi.xz.com.smartcoupon.R;
 import publi.xz.com.smartcoupon.base.BaseActivity;
+import publi.xz.com.smartcoupon.utils.GlideImageLoader;
 
-public class MainActivity extends BaseActivity implements View.OnClickListener{
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private Button test;
+    private Banner banner;
 
 
     @Override
@@ -21,17 +30,37 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     @Override
     public void init_Data() {
         findID();
+        //设置banner轮播图
+        init_banner();
+
+
+    }
+
+    private void init_banner() {
+        List<String> img=new ArrayList<>();
+        img.add("https://s2.ax1x.com/2019/07/09/ZyGEHH.png");
+        img.add("https://s2.ax1x.com/2019/07/09/ZyGQv8.png");
+        img.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic259ohaj30ci08c74r.jpg");
+        img.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic2b16zuj30ci08cwf4.jpg");
+        img.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic2e7vsaj30ci08cglz.jpg");
+        banner.setImages(img)
+                .setImageLoader(new GlideImageLoader())
+                .setDelayTime(3000)
+                .start();
+        
+
     }
 
     private void findID() {
         test = findViewById(R.id.test);
         test.setOnClickListener(this);
+        banner = findViewById(R.id.banner);
     }
 
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.test:
                 startActivity(new Intent(MainActivity.this, Top100Activity.class));
                 break;
